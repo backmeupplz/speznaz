9df594b79571a26cd540825247b31903276db96a
backmeupplz <backmeupplz@gmail.com>
2019-03-23 21:44:00 -0700

    finish charts

--- ToDo.swift
+++
@@ -1,21 +0,0 @@
-//
-//  ToDo.swift
-//  SpeznazDemo
-//
-//  Created by Nikita Kolmogorov on 2019-03-21.
-//  Copyright © 2019 Nikita Kolmogorov. All rights reserved.
-//
-
-import Foundation
-
-// - Fix selection values
-
-// + Time labels and grid labels animation
-// + Selection bubble (draggable)
-// + Time labels
-// + compute — get Y values only on the screen
-// + Chart scroll view
-// + Chart selection animation
-// + Range selection animation
-// + Range scrolling animation
-// + Chart scrolling animation
